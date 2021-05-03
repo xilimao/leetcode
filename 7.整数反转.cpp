b812200eb05fd10b9b3534dab25ffd659a7cb9fd
xilimao <caxilimao@gmail.com>
2021-05-03 21:24:26 +0800

    7

---
+++ 7.整数反转.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+   int rev = 0;
+        while (x != 0) {
+            if (rev < INT_MIN / 10 || rev > INT_MAX / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+        return rev;
+    }
+};
+// @lc code=end
+

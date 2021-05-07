6be1f306db3381867e65e525f9bdfd677eff0fb3
xilimao <caxilimao@gmail.com>
2021-05-07 19:17:11 +0800

    1486

---
+++ 1486.数组异或操作.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1486 lang=cpp
+ *
+ * [1486] 数组异或操作
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ans = start;
+        for (int i = 1; i < n; i++)
+        {
+            ans ^= (start + i * 2);
+        }
+        
+        return ans;
+    }
+};
+// @lc code=end
+

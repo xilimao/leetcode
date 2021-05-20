80e975e45d548b6935e6c621a35694c5831ef86a
xilimao <caxilimao@gmail.com>
2021-05-20 22:30:07 +0800

    692-cv

---
+++ 692.前k个高频单词.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=692 lang=cpp
+ *
+ * [692] 前K个高频单词
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> cnt;
+        for (auto& word : words) {
+            ++cnt[word];
+        }
+        vector<string> rec;
+        for (auto& [key, value] : cnt) {
+            rec.emplace_back(key);
+        }
+        sort(rec.begin(), rec.end(), [&](const string& a, const string& b) -> bool {
+            return cnt[a] == cnt[b] ? a < b : cnt[a] > cnt[b];
+        });
+        rec.erase(rec.begin() + k, rec.end());
+        return rec;
+    }
+};
+
+// @lc code=end
+
